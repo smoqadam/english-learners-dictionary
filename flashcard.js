f5fc42a901d25b5716185dd6e2ffa039c9235107
smoqadam <phpro.ir@gmail.com>
2017-09-11 21:20:27 +0430

    add preventDefault() to Translate anchor tag

--- flashcard.js
+++ flashcard.js
@@ -1,4 +1,3 @@
-document.body.style.border = "5px solid red";
 var translator = document.createElement('div');
 translator.style.display = "none";
 translator.style.position = "fixed";
@@ -12,47 +11,63 @@ translator.style.margin = "5px";
 translator.style.maxWidth = "500px";
 document.body.appendChild(translator);
 
-var menu = document.createElement("div");
+var menu = document.createElement("ul");
 menu.style.background = "#ccc";
-menu.width = "20px";
+menu.style.listStyleType  = "none";
+menu.style.textAlign = "center";
 menu.style.position = "absolute";
-
-var menudItemTranslate = document.createElement('div');
+menu.style.zIndex = "9999";
+// menu.style.borderRadius = "50%";
+// menu.style.width = "20px";
+// menu.style.height = "20px";
 var selection;
-menudItemTranslate.style.background = "#ccc";
-menudItemTranslate.style.padding = "5px";
-menudItemTranslate.textContent = "Tr";
-menudItemTranslate.addEventListener('click', function(e){
+
+var menudItemTranslate = document.createElement('li');
+menu.appendChild(menudItemTranslate);
+var menudItemTranslateA = document.createElement('a');
+menudItemTranslateA.style.padding = "5px";
+menudItemTranslateA.style.display = "inline";
+menudItemTranslateA.style.cursor = "pointer";
+menudItemTranslateA.textContent = "Tr";
+menudItemTranslateA.href = "#";
+menudItemTranslateA.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log("selection");
+  console.log(selection);
   if (selection !== ''){
-    translator.textContent = selection;
-    translator.style.display = "block";
-    menu.style.display = "none";
+        translator.textContent = selection;
+        translator.style.display = "block";
+        menu.style.display = "none";
   }
 });
+menudItemTranslate.appendChild(menudItemTranslateA);
 
-var menudItemAdd = document.createElement('div');
-menudItemAdd.addEventListener('click', function(e){
-  //do something
-});
-menudItemAdd.style.background = "#ccc";
-menudItemAdd.style.padding = "5px";
-menudItemAdd.textContent = "Add";
+//TODO: add functionality
+// var menudItemAdd = document.createElement('li');
+// menudItemAdd.style.display = "inline";
+// menudItemAdd.style.background = "#ccc";
+// menudItemAdd.style.padding = "5px";
+// menudItemAdd.textContent = "Add";
+// menudItemAdd.addEventListener('click', function(e){
+//   //do something
+// });
+// menu.appendChild(menudItemAdd);
 
-menu.appendChild(menudItemAdd);
-menu.appendChild(menudItemTranslate);
 document.body.appendChild(menu);
 
 
 document.onmouseup =  function (evt) {
   var s = document.getSelection(),
+  bodyRect = document.body.getBoundingClientRect(),
   r = s.getRangeAt(0);
   if (r && s.toString()) {
     var p = r.getBoundingClientRect();
     if (p.left || p.top) {
       menu.style.left = (p.left) +"px";
-      menu.style.top = (p.top - menu.offsetHeight - 10) +"px";
+      menu.style.top = ((p.top - bodyRect.top) - menu.offsetHeight - 10) +"px";
       menu.style.display = "block";
       selection = s.toString();
+      console.log(selection);
       return;
     }
   }
