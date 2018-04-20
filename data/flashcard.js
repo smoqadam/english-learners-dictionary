177ca1ba53b5897433732d35f30825b1822ab410
smoqadam <phpro.ir@gmail.com>
2018-04-20 17:00:32 +0430

    fix styles and bugs

--- data/flashcard.js
+++ data/flashcard.js
@@ -1,34 +1,36 @@
-let translateButton = '<div class="tr-wrapper" style="position: absolute"><a href="#" class="tr-button">Tr.</a></div>';
-let style = '<style>' +
-    '.tr-loading {display:none;z-index: 99999999; background: #2092cc; padding:10px;color:#fff; position: fixed; top:5px; left: 5px; } ' +
-    '.tr-body{display:none;z-index: 99999999; background: #2092cc; padding:10px;color:#fff; position: fixed; top:5px; left: 5px; width: 450px; height: 250px; overflow-y: scroll;} ' +
-    '.tr-body ul{ list-style: disc }' +
-    '.tr-pos {font-weight: bold; clear:both;}' +
-    '.tr-close {position:absolute; right:0; top: 0; padding: 5px; background-color: red; cursor:pointer; width: 15px; height: 15px; border-radius: 15px;}' +
-    '</style>' ;
+let translateButton = '<div class="tr-wrapper"><div class="tr-button"></div></div>';
 
 let translationBody =
-    '<div style="overflow: hidden;" class="tr-def-wrapper">' +
-    '<div class="tr-body" style="">' +
-    '<div class="tr-close">x</div> ' +
+    '<div style="" class="tr-def-wrapper">' +
+    '<div class="tr-body">' +
+    '<span class="tr-close fat"></span>     ' +
     '<h4 class="tr-word"></h4>' +
     '<h6 class="tr-pos"></h6>' +
     '<span class="tr-pron"></span><br>' +
     '<ul class="tr-defs"></ul>' +
     '</div></div>';
 
-let loading = '<div style="display: none;" class="tr-loading">Please Wait...</div>';
-$(style).appendTo('head');
+let loading = '<div class="tr-loading">Please Wait...</div>';
 $(translateButton).appendTo('body');
 $(translationBody).appendTo('body');
 $(loading).appendTo('body');
 let selection;
+$('.tr-body').click(function (e) {
+    e.stopPropagation();
+});
+$(document).click(function (e) {
+    $('body').find('.tr-body').css('display', 'none');
+});
+
+$('.tr-close').click(function(){
+    $('body').find('.tr-body').css('display', 'none');
+});
 
 $('.tr-button').on('click', function(e){
     e.preventDefault();
     $('.tr-wrapper').css('display', 'none');
     if (selection !== ''){
-        let url = "http://127.0.0.1:8000/"+selection;
+        let url = "http://dict.smoqadam.me/"+selection;
         const myRequest = new Request(url);
         $('body').find('.tr-body').css('display', 'none');
         $('body').find('.tr-loading').css('display','block');
@@ -45,11 +47,12 @@ $('.tr-button').on('click', function(e){
                 $('body').find('.tr-body .tr-pos').html(response['parts_of_speech']);
                 $('body').find('.tr-body .tr-pron').html(response['pronunciation']);
                 $('body').find('.tr-body .tr-word').html(response['word']);
+                $('body').find('.tr-body ul.tr-defs').html('')
                 let def;
                 for([index, definition] of Object.entries(response.definitions)){
-                    def = $('<li class="tr-def">'+definition.definition+'<ul></ul></li>');
+                    def = $('<li class="tr-def">'+definition.definition+'</li>');
+                    $(def).append('<ul></ul>');
                     for ([key, example] of Object.entries(definition.examples)){
-                        console.log(example);
                         $(def).find('ul').append('<li class="examples">'+example.example+'</li>')
                     }
                     $('body').find('.tr-body ul.tr-defs').append(def);
@@ -75,9 +78,8 @@ document.onmouseup =  function (evt) {
         let p = r.getBoundingClientRect();
         if (p.left || p.top) {
             selection = s.toString();
-            console.log(selection);
             $(document).find('.tr-wrapper').css({
-                top: ((p.top - bodyRect.top) - 20) + "px",
+                top: ((p.top - bodyRect.top) - 25) + "px",
                 left: p.left + "px",
                 display: "block",
             });
